96f3ec480aba0675f57a37ee01b1ceab916e02f0
Vamsi-134 <vamsikannemadugu1334@gmail.com>
2026-03-20 16:14:54 +0530

    final db connection

--- src/main/java/com/student/student/DBConnection.java
+++ src/main/java/com/student/student/DBConnection.java
@@ -1,32 +1,25 @@
-package com.student.student;
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
 
     public static Connection getConnection() {
-
         Connection con = null;
 
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/college",
-                    "root",
-                    "Mahi@Java2026#"   // your mysql password
+                "jdbc:mysql://hopper.proxy.rlwy.net:30375/railway?useSSL=false&allowPublicKeyRetrieval=true",
+                "root",
+                "fNOhtFhxDVQDINvjMuyDRJHmynyNAkvl"
             );
 
             System.out.println("Database Connected Successfully");
 
         } catch (Exception e) {
-
             System.out.println("Database connection failed");
             e.printStackTrace();
-
         }
 
         return con;
